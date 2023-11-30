f0756c5417408959cb5d48d5472d18ea470df98d
jguimeram <jguimera54@gmail.com>
2023-11-30 07:45:13 +0100

    remove Item class. Game class up to date

--- js/Game.js
+++ js/Game.js
@@ -1,32 +1,58 @@
 export default class Game {
 
-    playerOption = null
+    playerOption = 0
+    AIOption = 0
+    playerPoints = 0
+    AIPoints = 0
+    items = [
+        { id: 0, name: 'rock', counter: 1, win: 2 },
+        { id: 1, name: 'paper', counter: 2, win: 0 },
+        { id: 2, name: 'scissors', counter: 0, win: 1 },
+    ]
+    res
 
     constructor() {
         this.playerSelection = document.querySelector('.board__icons')
+        this.score = document.querySelector('.board__score')
         this.setPlayerTurn()
-        this.playerGame()
     }
+
+    setAITurn() {
+        this.AIOption = Math.floor(Math.random() * 3)
+        if (this.AIOption >= 3) throw new ('wrong AI Selection: ', option)
+    }
+
 
     setPlayerTurn() {
         this.playerSelection.addEventListener('click', (e) => {
             if (e.target.classList.contains('icons')) {
                 this.playerOption = e.target.dataset.id
+                this.setAITurn()
+                this.setResult()
             }
         })
     }
 
 
+    setResult() {
 
-    playerGame() {
-        console.log(this.playerOption);
+        let res = this.items.find((e) => {
+            return e.id === parseInt(this.playerOption)
+        })
+
+        console.log(`Player: ${this.playerOption}, AI: ${this.AIOption}`);
+
+        if (res.counter === this.AIOption) {
+            this.AIPoints++ //win AI
+            this.score.children[1].children[0].textContent = this.AIPoints
+        }
+        else if (res.win === this.AIOption) {
+            this.playerPoints++ //win player
+            this.score.children[0].children[0].textContent = this.playerPoints
+        }
+        else this.point = -1 //tie
+
     }
 
-
-    AITurn() {
-        let option = Math.floor(Math.random() * 3)
-        if (option >= 3) throw new ('wrong AI Selection: ', option)
-        return option
-    }
 
 }
--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
-import Item from "./Item.js"
 import Game from "./Game.js"
-
-const item = new Item()
 const game = new Game()
+
+
 
 
 
