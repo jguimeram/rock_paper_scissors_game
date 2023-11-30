bc97d705a528e4b4dc9d91f3a6f86cd3036aa281
jguimeram <jguimera54@gmail.com>
2023-11-30 15:15:43 +0100

    score works fine

--- js/Game.js
+++ js/Game.js
@@ -9,17 +9,19 @@ export default class Game {
         { id: 1, name: 'paper', counter: 2, win: 0 },
         { id: 2, name: 'scissors', counter: 0, win: 1 },
     ]
-    res
+
 
     constructor() {
         this.playerSelection = document.querySelector('.board__icons')
         this.score = document.querySelector('.board__score')
+        this.actions = document.querySelector('.actions')
         this.setPlayerTurn()
     }
 
     setAITurn() {
         this.AIOption = Math.floor(Math.random() * 3)
         if (this.AIOption >= 3) throw new ('wrong AI Selection: ', option)
+        this.setResult()
     }
 
 
@@ -28,7 +30,7 @@ export default class Game {
             if (e.target.classList.contains('icons')) {
                 this.playerOption = e.target.dataset.id
                 this.setAITurn()
-                this.setResult()
+
             }
         })
     }
@@ -40,7 +42,6 @@ export default class Game {
             return e.id === parseInt(this.playerOption)
         })
 
-        console.log(`Player: ${this.playerOption}, AI: ${this.AIOption}`);
 
         if (res.counter === this.AIOption) {
             this.AIPoints++ //win AI
@@ -52,7 +53,15 @@ export default class Game {
         }
         else this.point = -1 //tie
 
+        /* console.log(`Player: ${this.playerOption}, AI: ${this.AIOption}`); */
+        this.printAction()
     }
 
+    printAction() {
+        const action = document.createElement('p')
+        action.classList.add('actions--p')
+        this.actions.appendChild(action)
+        action.textContent = `${this.items[this.playerOption].name} - ${this.items[this.AIOption].name}`
+    }
 
 }
